399476da9908a93fe28b18fa410514f716f15eee
Jhakailash <kailashjhaji7@gmail.com>
2022-06-06 13:31:36 +0530

    payment_card

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import Card from './components/Card';
+import Apple from './components/Apple';
+
+function App() {
+  return (
+
+    <div className="App">
+    <br></br>
+
+    <Card></Card>
+    <br></br>
+    <br></br>
+    <br></br>
+
+    <Card></Card>
+    <br></br>
+    <br></br>
+    <br></br>
+    
+    <Apple></Apple>
+     
+    </div>
+  );
+}
+
+export default App;
